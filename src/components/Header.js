fa890564ef73ffef57d96850c1607d2cd5259046
Rahul Dubey <rahuldubay51@gmail.com>
2024-05-01 00:12:38 +0530

    Applying React Best Practices

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,13 +7,14 @@ import { useDispatch } from "react-redux";
 import { addUser, removeUser } from "../../src/utils/userSlice";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { LOGO } from "../../src/utils/constants";
 const Header = () => {
   let navigate = useNavigate();
   let userInformationFromStore = useSelector((store) => store?.user);
   const dispatch = useDispatch();
   //* User Authentication
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+   let unsubscribe =  onAuthStateChanged(auth, (user) => {
       if (user) {
         const { uid, email, displayName } = user;
         //! Data stores in redux store
@@ -23,7 +24,10 @@ const Header = () => {
         dispatch(removeUser());
         navigate("/");
       }
-    });
+   });
+    return () => {
+      unsubscribe();
+    };
   }, []);
   //* Function that logs out of the Application
   const handleLogout = () => {
@@ -38,11 +42,7 @@ const Header = () => {
 
   return (
     <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-50 flex justify-between w-screen">
-      <img
-        className="w-44"
-        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-        alt="logo"
-      />
+      <img className="w-44" src={LOGO} alt="logo" />
       {userInformationFromStore && (
         <div>
           <button onClick={handleLogout} className="p-4 text-2xl text-blue-950">
